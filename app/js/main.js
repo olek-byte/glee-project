64299aa3cccd8d41c41278d231754d3962660280
olek-byte <olekhyzhniak@gmail.com>
2020-12-11 00:08:44 +0200

    fixed header

--- app/js/main.js
+++ app/js/main.js
@@ -2,9 +2,8 @@ $(function(){
 
     $('.menu__nav-btn, .menu a').on('click', function(){
    		 $('.header__inner').toggleClass('header__inner--active')
- 	  });
-
-
+     });
+     
 
   $('.top-slider__inner').slick({
     dots: true,
@@ -14,6 +13,12 @@ $(function(){
     // autoplaySpeed: 3000
 
   });
+
+//        $(".menu__list li ul").hide(); // скрываем выпадающее меню
+//     $(".menu__list li:has('.menu__submenu')").hover(
+//   function(){
+//   $(".menu__list li ul").stop().fadeToggle(0);} /* отбираем элемент списка, который содержит элемент с классом .submenu и добавляем ему функцию при наведении, которая показывает и скрывает выпадающее меню */
+// );
 
 
 
