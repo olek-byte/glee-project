400c214f9d02969b0eb75f61a89d0b5a5219b815
olek-byte <olekhyzhniak@gmail.com>
2020-12-15 22:05:08 +0200

    fixed page

--- app/js/main.js
+++ app/js/main.js
@@ -1,28 +1,76 @@
 $(function(){
 
-    $('.menu__nav-btn, .menu a').on('click', function(){
-   		 $('.header__inner').toggleClass('header__inner--active')
-     });
-     
+  // console.log("rabotaet!");
 
+  // Nav button click
+  $('.menu__nav-btn').on('click', function(){
+   	$('.header__inner').toggleClass('header__inner--active')
+  });
+
+
+  // Fixed header
+  let header = $('.header');
+  let topSlider = $('.top-slider');
+  let topSliderH = topSlider.innerHeight();
+  let scrollPos = $(window).scrollTop();
+
+  $(window).on("scroll", function(){
+    scrollPos = $(this).scrollTop();
+    console.log(scrollPos);
+
+    if( scrollPos > topSliderH ) {
+      header.addClass('fixed');
+      } else {
+        header.removeClass('fixed');
+      }
+
+  });
+
+
+  // Slider
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
     // fade: true,
     // autoplay: true,
     // autoplaySpeed: 3000
-
   });
 
-//        $(".menu__list li ul").hide(); // скрываем выпадающее меню
-//     $(".menu__list li:has('.menu__submenu')").hover(
-//   function(){
-//   $(".menu__list li ul").stop().fadeToggle(0);} /* отбираем элемент списка, который содержит элемент с классом .submenu и добавляем ему функцию при наведении, которая показывает и скрывает выпадающее меню */
-// );
+
+  $('.partners__list').slick({
+    // settings: "unslick",
+  // dots: false,
+  // infinite: false,
+  slidesToShow: 5,
+  slidesToScroll: 5,
+    dots: true,
+        arrows: false,
+    responsive: [
+     {
+      breakpoint: 993,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+        arrows: false,
+        
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+    ]
+  });
 
 
+ 
 
+  // Mixitup
   var mixer = mixitup('.products__inner');
-  
   var mixer = mixitup('.design__inner');
+
 });
